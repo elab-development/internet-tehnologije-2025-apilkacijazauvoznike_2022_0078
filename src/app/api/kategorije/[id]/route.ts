690f13217f67efec06a84247d4ddd5abe4e0b3af
Petar Lovic <242365578+pl20220450@users.noreply.github.com>
2026-01-24 17:28:48 +0100

    Api za kategorije-CRUD

---
+++ src/app/api/kategorije/[id]/route.ts
@@ -0,0 +1,59 @@
+import { NextResponse } from "next/server";
+import { deleteCategoryById, updateCategory } from "@/src/controllers/kategorije_controller";
+
+
+export async function DELETE(req: Request, context: any) {
+  try {
+
+    const params = await context.params;
+    const id = Number(params.id);
+
+    const result = await deleteCategoryById(id);
+
+    if (result.error) {
+      const statusCode = result.error.code === "NOT_FOUND" ? 404 : 400;
+      return NextResponse.json(
+        { ok: false, error: result.error.code, message: result.error.message },
+        { status: statusCode }
+      );
+    }
+
+    return NextResponse.json({ ok: true, data: result.data });
+  } catch (e) {
+    return NextResponse.json(
+      { ok: false, error: "INTERNAL_ERROR", message: "Greska pri brisanju kategorije!" },
+      { status: 500 }
+    );
+  }
+}
+
+
+export async function PATCH(req: Request, context: any) {
+  try {
+
+    const params = await context.params;
+    const id = Number(params.id);
+    const body = await req.json();
+
+    const result = await updateCategory(id, {
+      ime: body.ime,
+      opis: body.opis,
+    });
+
+    if (result.error) {
+      let status = 400;
+      if (result.error.code === "NOT_FOUND") status = 404;
+      if (result.error.code === "NO_CHANGES") status = 409; 
+
+      return NextResponse.json({ ok: false, ...result.error }, { status });
+    }
+
+    return NextResponse.json({ ok: true, data: result.data });
+  } catch (e: any) {
+    return NextResponse.json(
+      { ok: false, error: "INTERNAL_ERROR", message: "Greska pri izmeni podataka!" },
+      { status: 500 }
+    );
+  }
+}
+
---
+++ src/controllers/kategorije_controller.ts
@@ -0,0 +1,97 @@
+import { db } from "@/src/db";
+import { kategorija } from "@/src/db/schema";
+import { eq } from "drizzle-orm";
+
+
+export async function getAllCategories() {
+  const data = await db.select().from(kategorija);
+  return data;
+}
+
+
+export async function createCategory(input: { ime: string; opis?: string | null }) {
+  const ime = input.ime?.trim();
+  const opis = input.opis ?? null;
+
+  // Validacija
+  if (!ime) {
+    return { error: { code: "VALIDATION_ERROR", message: "Polje ime je obavezno!" }};
+  }
+
+  // Ubacivanje u bazu
+  const inserted = await db.insert(kategorija).values({ ime, opis }).returning();
+
+  return { data: inserted[0] };
+}
+
+export async function deleteCategoryById(id: number) {
+  // Validacija
+  if (!Number.isInteger(id) || id <= 0) {
+    return { error: { code: "VALIDATION_ERROR", message: "ID mora biti pozitivan broj!" }};
+  }
+
+  // Brisanje iz baze
+  const deleted = await db.delete(kategorija).where(eq(kategorija.id, id)).returning();
+
+  if (deleted.length === 0) {
+    return { error: { code: "NOT_FOUND", message: "Kategorija nije pronadjena!" }};
+  }
+
+  return { data: deleted[0] };
+}
+
+export async function updateCategory(id: number, input: { ime?: string; opis?: string | null }) {
+  // Validacija 
+  if (!Number.isInteger(id) || id <= 0) {
+    return { error: { code: "VALIDATION_ERROR", message: "ID mora biti pozitivan broj!" } };
+  }
+
+  
+  const ime = input.ime?.trim();
+  const opis = input.opis;
+
+  // Da li su poslati podaci prazni?
+  if (ime === undefined && opis === undefined) {
+    return { 
+      error: { code: "VALIDATION_ERROR", message: "Morate poslati bar jedno polje (ime ili opis)!" } 
+    };
+  }
+
+  // Vraca kategoriju za izmenu iz baze
+  const existing = await db
+    .select()
+    .from(kategorija)
+    .where(eq(kategorija.id, id))
+    .limit(1);
+
+  if (existing.length === 0) {
+    return { error: { code: "NOT_FOUND", message: "Kategorija nije pronađena!" } };
+  }
+
+  const kategorijaBaza = existing[0];
+
+  // Uporedjujemo podatke kategorije iz baze sa unetim podacima
+  const finalIme = ime !== undefined ? ime : kategorijaBaza.ime;
+  const finalOpis = opis !== undefined ? opis : kategorijaBaza.opis;
+
+  if (finalIme === kategorijaBaza.ime && finalOpis === kategorijaBaza.opis) {
+    return { 
+      error: { 
+        code: "NO_CHANGES", 
+        message: "Nema razlike između poslatih i postojećih podataka!" 
+      } 
+    };
+  }
+
+  // Azuriranje
+  const updated = await db
+    .update(kategorija)
+    .set({ 
+      ime: finalIme, 
+      opis: finalOpis 
+    })
+    .where(eq(kategorija.id, id))
+    .returning();
+
+  return { data: updated[0] };
+}
